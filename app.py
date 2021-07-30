643724234bc9340acbecdb26b4b3eb23d86bdb47
Yusuffrih <yusuffrih@hotmail.com>
2021-07-30 19:33:12 +0000

    Addded logout functionality

--- app.py
+++ app.py
@@ -52,19 +52,19 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # check to see if username exists in db
-        exisiting_user = mongo.db.users.find_one(
-            {"username": request.form.get("username".lower())})
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
 
-        if exisiting_user:
-            # exisiting hashed password matches users input
+        if existing_user:
+            # ensure hashed password matches user input
             if check_password_hash(
-                exisiting_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(
-                        url_for("profile", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -73,8 +73,17 @@ def login():
         else:
             # username doesn't exist
             flash("Incorrect Username and/or Password")
-            redirect(url_for("login"))
+            return redirect(url_for("login"))
+
     return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 @app.route("/profile/<username>", methods=["GET", "POST"])
@@ -82,7 +91,10 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
